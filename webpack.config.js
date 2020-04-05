47e3d79298b0193d842091d73bc2a9c8360c599a
Narinder Singh Saund <nssaund@juno.com>
2020-04-04 19:09:00 -0700

    completed large hero block

--- webpack.config.js
+++ webpack.config.js
@@ -7,15 +7,24 @@ const postCSSPlugins = [
     require('autoprefixer')
 ]
 
-
 module.exports ={
     entry: './app/assets/scripts/App.js',
     output: {
         filename: 'bundled.js',
         path: path.resolve(__dirname, 'app')
     },
+    devServer: {
+        before: function(app, server) {
+            server._watch('./app/**/*.html')
+        },
+
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000
+    },
+
     mode: 'development',
-    watch: true,
+    
     module: {
         rules: [
             {
